2836bc4e2a194e5643d8e7815432f8a66d0ac4a2
Patrick <patrickwarley@gmail.com>
2023-07-18 22:15:03 -0300

    update on car card

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import { Hero, SearchBar, CustomFilter } from '@/components';
+import { Hero, SearchBar, CustomFilter, CarCard } from '@/components';
 import { fetchCars } from '@/utils';
 import Image from 'next/image';
 
 export default async function Home() {
 
 	const allCars = await fetchCars();
-	console.log(allCars);
+
+	const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+	console.log(allCars, isDataEmpty)
 	return (
 		<main className="overflow-hidden ">
 			<Hero />
@@ -21,10 +24,21 @@ export default async function Home() {
 						<CustomFilter title="year" />
 					</div>
 				</div>
+				{
+					!isDataEmpty ? (
+						<section>
+							<div className='home_cars-wrapper'>
+								{allCars.map((car) => <CarCard car={car} />)}
+							</div>
+						</section>
+					) : (
+						<div className='home_error-container'>
+							<h2 className='text-black text-xl font-bold'>Oops, no results.</h2>
+							<p>{allCars?.message}</p>
+						</div>
 
-				<div className='home_cars-wrapper'>
-					{allCars.map((car) => <div>{car.model}</div>)}
-				</div>
+					)
+				}
 
 			</div>
 		</main>
--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import { CarProps } from '@/types'
+import Image from 'next/image';
 
-const CarCard = ({ }) => {
+interface CarCardProps {
+	car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+	const { city_mpg, year, make, model, transmission, drive } = car;
+
 	return (
-		<div className='car-card'>
+		<div className='car-card group'>
 			<div className="car-card_content">
-				<div className="car-card_content-title"></div>
-				<div className="car-card_price"></div>
-				<div className="car-card_price-dollar"></div>
-				<div className="car-card_price-day"></div>
-				<div className="car-card_image"></div>
-				<div className="car-card_icon-container"></div>
-				<div className="car-card_icon"></div>
-				<div className="car-card_icon-text"></div>
-				<div className="car-card_btn-container"></div>
+				<h2 className="car-card_content-title">{make} {model}</h2>
 			</div>
-		</div>
+
+			<p className="car-card_price">
+				<span className="car-card_price-dollar">$</span>
+				<div>52</div>
+				<span className="car-card_price-day">/day</span>
+			</p>
+
+			<div className="car-card_image">
+				<Image src="/hero.png" fill priority alt="car image" />
+			</div>
+			<div className="car-card_icon-container"></div>
+			<div className="car-card_icon"></div>
+			<div className="car-card_icon-text"></div>
+			<div className="car-card_btn-container"></div>
+		</div >
 	)
 }
 
